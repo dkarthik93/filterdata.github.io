dd526e36defe3c584051878c3e5a5832d1994254
karthik <dk.karthikd9@gmail.com>
2022-11-30 22:35:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MaterialTable from 'material-table';
+import * as XLSX from 'xlsx';
 
+const EXTENSIONS = ['xlsx', 'xls', 'csv']
 function App() {
+  const [colDefs, setColDefs] = useState()
+  const [data, setData] = useState()
+
+  const getExention = (file) => {
+    const parts = file.name.split('.')
+    const extension = parts[parts.length - 1]
+    return EXTENSIONS.includes(extension) // return boolean
+  }
+
+  const convertToJson = (headers, data) => {
+    const rows = []
+    data.forEach(row => {
+      let rowData = {}
+      row.forEach((element, index) => {
+        rowData[headers[index]] = element
+      })
+      rows.push(rowData)
+
+    });
+    return rows
+  }
+
+  const importExcel = (e) => {
+    const file = e.target.files[0]
+
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      
+      //parse data
+
+      const bstr = event.target.result
+      const workBook = XLSX.read(bstr, { type: "binary" })
+
+      //get first sheet
+      const workSheetName = workBook.SheetNames[0]
+      const workSheet = workBook.Sheets[workSheetName]
+      //convert to array
+      const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
+      // console.log(fileData)
+      const headers = fileData[0]
+      const heads = headers.map(head => ({ title: head, field: head }))
+      setColDefs(heads)
+
+      //removing header
+      fileData.splice(0, 1)
+
+
+      setData(convertToJson(headers, fileData))
+    }
+
+    if (file) {
+      if (getExention(file)) {
+        reader.readAsBinaryString(file)
+      }
+      else {
+        alert("Invalid file input, Select Excel, CSV file")
+      }
+    } else {
+      setData([])
+      setColDefs([])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 align="center">React-Data-Filter-App</h1>
+      <h4 align='center'>Import Data from Excel, CSV in Material Table</h4>
+      <input type="file" onChange={importExcel} />
+      <MaterialTable title="Employee Data" data={data} columns={colDefs}/> 
     </div>
   );
 }
